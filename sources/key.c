ebf7c603a10a68c9c6f92b78ca1c0760e90d4df3
Etheram <francois681927@hotmail.fr>
2019-11-30 12:20:15 +0100

    no update

--- sources/key.c
+++ sources/key.c
@@ -6,7 +6,7 @@
 /*   By: frfrey <frfrey@student.le-101.fr>          +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/11/23 10:04:14 by frfrey       #+#   ##    ##    #+#       */
-/*   Updated: 2019/11/30 10:36:37 by frfrey      ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/11/30 11:54:46 by frfrey      ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -21,5 +21,9 @@ int		deal_key(int key, t_map *map)
 		map->player.pos.x -= 0.1;
 	if (key == 1)
 		map->player.pos.x += 0.1;
+	if (key == 0)
+		map->player.pos.y -= 0.1;
+	if (key == 2)
+		map->player.pos.y += 0.1;
 	return (1);
 }
