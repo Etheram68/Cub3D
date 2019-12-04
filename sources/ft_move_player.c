f4cfd315097ab496c11f656c291b767d976606b0
Etheram <francois681927@hotmail.fr>
2019-12-04 13:15:30 +0100

    Add modification gestion colision

--- sources/ft_move_player.c
+++ sources/ft_move_player.c
@@ -6,7 +6,7 @@
 /*   By: frfrey <frfrey@student.le-101.fr>          +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/12/04 11:25:34 by frfrey       #+#   ##    ##    #+#       */
-/*   Updated: 2019/12/04 11:50:16 by frfrey      ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/12/04 13:15:02 by frfrey      ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -17,20 +17,20 @@ void	ft_move_up_down(t_map *map)
 {
 	if (PLAYER.w == 1)
 	{
-		if (map->map[(int)(PLAYER.pos.x + PLAYER.pos.x * PLAYER.speed_move)]
-						[(int)PLAYER.pos.y] == 0)
+		if (!(map->map[(int)(PLAYER.pos.x + PLAYER.pos.x * PLAYER.speed_move)]
+						[(int)PLAYER.pos.y] == 1))
 			PLAYER.pos.x += PLAYER.dir.x * PLAYER.speed_move;
-		if (map->map[(int)PLAYER.pos.x]
-			[(int)(PLAYER.pos.y + PLAYER.dir.y * PLAYER.speed_move)] == 0)
+		if (!(map->map[(int)PLAYER.pos.x]
+			[(int)(PLAYER.pos.y + PLAYER.dir.y * PLAYER.speed_move)] == 1))
 			PLAYER.pos.y += PLAYER.dir.y * PLAYER.speed_move;
 	}
 	if (PLAYER.s == 1)
 	{
-		if (map->map[(int)(PLAYER.pos.x + PLAYER.dir.x * PLAYER.speed_move)]
-			[(int)PLAYER.pos.y] == 0)
+		if (!(map->map[(int)(PLAYER.pos.x + PLAYER.dir.x * PLAYER.speed_move)]
+			[(int)PLAYER.pos.y] == 1))
 			PLAYER.pos.x -= PLAYER.dir.x * PLAYER.speed_move;
-		if (map->map[(int)PLAYER.pos.x]
-			[(int)(PLAYER.pos.y + PLAYER.dir.y * PLAYER.speed_move)] == 0)
+		if (!(map->map[(int)PLAYER.pos.x]
+			[(int)(PLAYER.pos.y + PLAYER.dir.y * PLAYER.speed_move)] == 1))
 			PLAYER.pos.y -= PLAYER.dir.y * PLAYER.speed_move;
 	}
 }
@@ -39,20 +39,20 @@ void	ft_move_left_rigth(t_map *map)
 {
 	if (PLAYER.a == 1)
 	{
-		if (map->map[(int)(PLAYER.pos.x + PLAYER.dir.y * PLAYER.speed_move)]
-			[(int)PLAYER.pos.y] == 0)
+		if (!(map->map[(int)(PLAYER.pos.x + PLAYER.dir.y * PLAYER.speed_move)]
+			[(int)PLAYER.pos.y] == 1))
 			PLAYER.pos.x -= PLAYER.dir.y * PLAYER.speed_move;
-		if (map->map[(int)PLAYER.pos.x]
-			[(int)(PLAYER.pos.y + PLAYER.dir.x * PLAYER.speed_move)] == 0)
+		if (!(map->map[(int)PLAYER.pos.x]
+			[(int)(PLAYER.pos.y + PLAYER.dir.x * PLAYER.speed_move)] == 1))
 			PLAYER.pos.y += PLAYER.dir.x * PLAYER.speed_move;
 	}
 	if (PLAYER.d == 1)
 	{
-		if (map->map[(int)(PLAYER.pos.x + PLAYER.dir.y * PLAYER.speed_move)]
-			[(int)PLAYER.pos.y] == 0)
+		if (!(map->map[(int)(PLAYER.pos.x + PLAYER.dir.y * PLAYER.speed_move)]
+			[(int)PLAYER.pos.y] == 1))
 			PLAYER.pos.x += PLAYER.dir.y * PLAYER.speed_move;
-		if (map->map[(int)PLAYER.pos.x]
-			[(int)(PLAYER.pos.y + PLAYER.dir.x * PLAYER.speed_move)] == 0)
+		if (!(map->map[(int)PLAYER.pos.x]
+			[(int)(PLAYER.pos.y + PLAYER.dir.x * PLAYER.speed_move)] == 1))
 			PLAYER.pos.y -= PLAYER.dir.x * PLAYER.speed_move;
 	}
 }
